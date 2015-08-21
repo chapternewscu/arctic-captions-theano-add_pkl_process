3e172d8148aa4939496b60ca146eca824af688aa
Anand Sampat <asampat3090@gmail.com>
2015-08-21 02:54:02 -0700

    add test and dev sets

--- make_flickr_data.py
+++ make_flickr_data.py
@@ -39,16 +39,71 @@ image_id_dict = pd.Series(np.array(images.index), index=images)
 caption_image_id = annotations['image'].map(lambda x: image_id_dict[x]).values
 cap = zip(captions, caption_image_id)
 
-for start, end in zip(range(0, len(images)+100, 100), range(100, len(images)+100, 100)):
-    image_files = images[start:end]
+# split up into train, test, and dev
+all_idx = np.random.shuffle(range(31784))
+train_idx  = all_idx[0:25000]
+test_idx = all_idx[25000:30000]
+dev_idx = all_idx[30000:]
+
+# training set
+images_train = images[train_idx]
+image_id_dict_train = image_id_dict[train_idx]
+caption_image_id_train = caption_image_id[train_idx]
+captions_train = captions[train_idx]
+cap_train = zip(captions_train, caption_image_id_train)
+
+for start, end in zip(range(0, len(images_train)+100, 100), range(100, len(images_train)+100, 100)):
+    image_files = images_train[start:end]
     feat = cnn.get_features(image_list=image_files, layers='conv5_3', layer_sizes=[512,14,14])
     if start == 0:
-        feat_flatten_list = scipy.sparse.csr_matrix(np.array(map(lambda x: x.flatten(), feat)))
+        feat_flatten_list_train = scipy.sparse.csr_matrix(np.array(map(lambda x: x.flatten(), feat)))
     else:
-        feat_flatten_list = scipy.sparse.vstack([feat_flatten_list, scipy.sparse.csr_matrix(np.array(map(lambda x: x.flatten(), feat)))])
+        feat_flatten_list_train = scipy.sparse.vstack([feat_flatten_list_train, scipy.sparse.csr_matrix(np.array(map(lambda x: x.flatten(), feat)))])
 
     print "processing images %d to %d" % (start, end)
 
 with open('data/flickr30k/flicker_30k_align.train.pkl', 'wb') as f:
-    cPickle.dump(cap, f)
-    cPickle.dump(feat_flatten_list, f)
+    cPickle.dump(cap_train, f)
+    cPickle.dump(feat_flatten_list_train, f)
+
+# test set
+images_test = images[test_idx]
+image_id_dict_test = image_id_dict[test_idx]
+caption_image_id_test = caption_image_id[test_idx]
+captions_test = captions[test_idx]
+cap_test = zip(captions_test, caption_image_id_test)
+
+for start, end in zip(range(0, len(images_test)+100, 100), range(100, len(images_test)+100, 100)):
+    image_files = images_test[start:end]
+    feat = cnn.get_features(image_list=image_files, layers='conv5_3', layer_sizes=[512,14,14])
+    if start == 0:
+        feat_flatten_list_test = scipy.sparse.csr_matrix(np.array(map(lambda x: x.flatten(), feat)))
+    else:
+        feat_flatten_list_test = scipy.sparse.vstack([feat_flatten_list_test, scipy.sparse.csr_matrix(np.array(map(lambda x: x.flatten(), feat)))])
+
+    print "processing images %d to %d" % (start, end)
+
+with open('data/flickr30k/flicker_30k_align.test.pkl', 'wb') as f:
+    cPickle.dump(cap_test, f)
+    cPickle.dump(feat_flatten_list_test, f)
+
+# dev set
+images_dev = images[dev_idx]
+image_id_dict_dev = image_id_dict[dev_idx]
+caption_image_id_dev = caption_image_id[dev_idx]
+captions_dev = captions[dev_idx]
+cap_dev = zip(captions_dev, caption_image_id_dev)
+
+for start, end in zip(range(0, len(images_dev)+100, 100), range(100, len(images_dev)+100, 100)):
+    image_files = images_dev[start:end]
+    feat = cnn.get_features(image_list=image_files, layers='conv5_3', layer_sizes=[512,14,14])
+    if start == 0:
+        feat_flatten_list_dev = scipy.sparse.csr_matrix(np.array(map(lambda x: x.flatten(), feat)))
+    else:
+        feat_flatten_list_dev = scipy.sparse.vstack([feat_flatten_list_dev, scipy.sparse.csr_matrix(np.array(map(lambda x: x.flatten(), feat)))])
+
+    print "processing images %d to %d" % (start, end)
+
+with open('data/flickr30k/flicker_30k_align.dev.pkl', 'wb') as f:
+    cPickle.dump(cap_dev, f)
+    cPickle.dump(feat_flatten_list_dev, f)
